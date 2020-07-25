27b86a61521a4a20e966fc2852056780054ffaa4
Aditya Bhardwaj <bhardwajaditya113@gmail.com>
2020-07-25 14:16:40 +0530

    Initial commit

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ import path from 'path';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import config from './config';
+
 
 // routes
 import authRoutes from './routes/api/auth';
 import itemRoutes from './routes/api/items';
 import userRoutes from './routes/api/users';
 
-const { MONGO_URI, MONGO_DB_NAME } = config;
 
 const app = express();
 
@@ -23,11 +22,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 // DB Config
-const db = `${MONGO_URI}/${MONGO_DB_NAME}`;
 
 // Connect to Mongo
 mongoose
-  .connect(db, {
+  .connect('mongodb://localhost:27017/shoppinglist', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -49,5 +47,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+
 
 export default app;
